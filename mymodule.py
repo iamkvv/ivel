614f55bbeb863a18861bf741ec43b1ab1fe381c3
iamkvv <vkondra@gmail.com>
2020-09-08 20:53:54 +0500

    Update mymodule.py

--- mymodule.py
+++ mymodule.py
@@ -1,19 +1,35 @@
 
-def revArr():
-    data=None
-    li=[]
-    i=0
-    while data != '':
-        data=input("ввод данных: ")
-        if data != "":
-           li.append(data)
+def getArray(): #Функция возвращает массив целых чисел, введенных с клав-ры
+    
+    data=None   #переменная для хранения введенного с клав-ры значения
+    li=[]       #массив по умолчанию пуст; будет заполняться с клав-ры в цикле 
 
+    while data != '': #выполнять, пока юзер не нажмет только Enter; 
+                      #тогда data станет равна '' и цикл закончится
+        data=input(f"Введено элементов - {len(li)}. Введите данные: ")
+        
+        if data != "" and str.isdigit(data): #если данные не пусты (только Enter) и все символы - цифры
+           li.append(int(data))              #преобразует строку в число и добавляет его в массив              
+           
+    return li #возвращает заполненный массив
+
+
+def revArray(li):
+    i=0
     while i < len(li) // 2:
         li[i], li[len(li)-1-i], i = li[len(li)-1-i], li[i], i+1
 
-    print(li)
-    li.reverse()
-    print(li)
+    return li
+
+
+def procArray():
+    li= getArray()
+    revli=revArray(li)
+
+    print(revli)
+    revli.reverse()
+    print(revli)
+
 
 """
 https://taskcode.ru/array/reverse
